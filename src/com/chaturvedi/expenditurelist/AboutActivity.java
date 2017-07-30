180634430f5c446b2a25b44d8671e8225d79baea
Nagabhushan S N <nagabhushansn.android@gmail.com>
2017-07-30 11:07:20 +0530

    ExpenditureList v2.2.7

--- src/com/chaturvedi/expenditurelist/AboutActivity.java
+++ src/com/chaturvedi/expenditurelist/AboutActivity.java
@@ -2,9 +2,15 @@ package com.chaturvedi.expenditurelist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build.VERSION;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.view.Window;
 import android.widget.RelativeLayout;
@@ -30,6 +36,14 @@ public class AboutActivity extends Activity
 		
 		TextView devotionView = (TextView)findViewById(R.id.devotion);
 		devotionView.setText("Devoted To Lord KRISHNA, My Parents, Teachers, Relatives, Friends And All Those Who Helped Me Reach This Position");
+		
+		TextView nagabhushanView = (TextView)findViewById(R.id.textView_nagabhushan);
+		SpannableString nagabhushanString = new SpannableString("NAGABHUSHAN S N, currently doing B.E. In PESIT, Bengaluru, India.");
+		nagabhushanString.setSpan(new RelativeSizeSpan(1.5f), 0, 15, 0);
+		nagabhushanString.setSpan(new ForegroundColorSpan(Color.parseColor("#FF00FF")), 0, 15, 0);
+		nagabhushanString.setSpan(new StyleSpan(2), 0, 15, 0);
+		nagabhushanString.setSpan(new UnderlineSpan(), 0, 15, 0);
+		nagabhushanView.setText(nagabhushanString);
 		
 		TextView contactView = (TextView)findViewById(R.id.contact);
 		contactView.setText("For Any Queries, Drop Me An Email At");
