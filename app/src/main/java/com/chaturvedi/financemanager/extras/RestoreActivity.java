9f60e7c36dc52e142af0657950a2ee0e53b406c9
Nagabhushan S N <nagabhushansn.android@gmail.com>
2017-07-30 14:42:55 +0530

    Finance Manager v4.2.1. Unnecessary files deleted and are no more tracked

---
+++ app/src/main/java/com/chaturvedi/financemanager/extras/RestoreActivity.java
@@ -0,0 +1,19 @@
+// Shree KRISHNAya Namaha
+
+package com.chaturvedi.financemanager.extras;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.chaturvedi.financemanager.R;
+
+public class RestoreActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_filter);
+	}
+}
